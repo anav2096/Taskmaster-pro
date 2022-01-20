5fbcb0bf59f2a7dade03a44513a48d36a9e7dca2
anav <anav2096@gmail.com>
2022-01-19 20:34:45 -0500

    this code sucks

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -1,5 +1,7 @@
 var tasks = {};
 
+
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
@@ -148,4 +150,67 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
+});
+
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  
+  update: function(event) {
+
+    var tempArr = [];
+    $(this).children().each(function() {
+      var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+      tasks[arrName] = tempArr;
+      saveTasks();
+      var text = $(this)
+      .find("p")
+      .text()
+      >trim();
+
+      var date = $(this)
+      .find("span")
+      .text()
+      .trim();
+
+      tempArr.push({
+        text: text,
+        date: date
+      });
+  
+      console.log(text, date);
+
+    }); 
+ }
+
+});
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
 });
